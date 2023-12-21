2791036fee29f113fe05603ef386c131d78c7042
Jeisonlr <jelurigi6@gmail.com>
2023-12-21 11:26:28 -0500

    primer commit

---
+++ src/test/java/PruebaTecnica/RetoSofka/Service/ClienteServiceImpTest.java
@@ -0,0 +1,147 @@
+package PruebaTecnica.RetoSofka.Service;
+import PruebaTecnica.RetoSofka.Exception.ApiRequestException;
+import PruebaTecnica.RetoSofka.Modelo.Cliente;
+import PruebaTecnica.RetoSofka.Modelo.Empleado;
+import PruebaTecnica.RetoSofka.Repository.ClienteRepository;
+import PruebaTecnica.RetoSofka.Repository.EmpleadoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ClienteServiceTest {
+
+    @Mock
+    private ClienteRepository clienteRepository;
+
+    @InjectMocks
+    private ClienteServiceImp clienteServiceImp;
+
+    @Test
+    void CrearCliente() throws ApiRequestException {
+
+        Cliente cliente = new Cliente(123,"Juan", "perez", "7788",
+                "7176621","j@.com",1.66,32,"juana perez",true);
+
+        when(clienteRepository.save(any(Cliente.class))).thenReturn(cliente);
+
+        Cliente resultado = clienteServiceImp.crearCliente(cliente);
+
+        verify(clienteRepository, times(1)).save(any(Cliente.class));
+        assertNotNull(resultado);
+        assertEquals(123, resultado.getId());
+        assertEquals("Juan", resultado.getNombre());
+        assertEquals("perez", resultado.getApellido());
+        assertEquals("7788", resultado.getCedula());
+        assertEquals("7176621", resultado.getTelefono());
+        assertEquals("j@.com", resultado.getCorreoElectronico());
+        assertEquals(1.66, resultado.getEstatura());
+        assertEquals(32, resultado.getEdad());
+        assertEquals("juana perez", resultado.getInfoContactoFamiliar());
+    }
+
+
+    @Test
+    void CrearClienteSinNombre() {
+        Cliente clienteSinNombre = new Cliente();
+        clienteSinNombre.setCedula("123456");
+        clienteSinNombre.setTelefono("987654321");
+        clienteSinNombre.setCorreoElectronico("cliente@gmail.com");
+        clienteSinNombre.setEstatura(1.75);
+        clienteSinNombre.setEdad(25);
+
+        ApiRequestException exception = assertThrows(ApiRequestException.class, () -> {
+            clienteServiceImp.crearCliente(clienteSinNombre);
+        });
+
+        assertNotNull(exception);
+
+        assertTrue(exception instanceof ApiRequestException);
+
+        if (exception.getMessage() != null) {
+            assertTrue(exception.getMessage().contains("El Cliente Debe Tener Un Nombre"));
+        }
+        verify(clienteRepository, never()).save(any(Cliente.class));
+    }
+
+    @Test
+    void CrearClienteSinApellido() {
+        Cliente clienteSinApellido = new Cliente();
+        clienteSinApellido .setNombre("Pedro Gaona");
+        clienteSinApellido .setCedula("123456");
+        clienteSinApellido .setTelefono("987654321");
+        clienteSinApellido .setCorreoElectronico("cliente@gmail.com");
+        clienteSinApellido .setEstatura(1.75);
+        clienteSinApellido .setEdad(25);
+
+        ApiRequestException exception = assertThrows(ApiRequestException.class, () -> {
+            clienteServiceImp.crearCliente(clienteSinApellido );
+        });
+
+        assertNotNull(exception);
+
+        assertTrue(exception instanceof ApiRequestException);
+
+        if (exception.getMessage() != null) {
+            assertTrue(exception.getMessage().contains("El Cliente Debe Tener Un Apellido"));
+        }
+        verify(clienteRepository, never()).save(any(Cliente.class));
+    }
+
+    @Test
+    void CrearClienteSinCedula() {
+        Cliente clienteSinCedula = new Cliente();
+        clienteSinCedula.setNombre("Pedro Gaona");
+        clienteSinCedula.setTelefono("987654321");
+        clienteSinCedula.setCorreoElectronico("cliente@gmail.com");
+        clienteSinCedula.setEstatura(1.75);
+        clienteSinCedula.setEdad(25);
+
+        ApiRequestException exception = assertThrows(ApiRequestException.class, () -> {
+            clienteServiceImp.crearCliente(clienteSinCedula);
+        });
+
+        assertNotNull(exception);
+
+        assertTrue(exception instanceof ApiRequestException);
+
+        if (exception.getMessage() != null) {
+            assertTrue(exception.getMessage().contains("El Cliente Debe Tener Una Cédula"));
+        }
+        verify(clienteRepository, never()).save(any(Cliente.class));
+    }
+
+
+    @Test
+    void CrearClienteMenorDeEdad() {
+        Cliente clienteMenorDeEdad = new Cliente();
+        clienteMenorDeEdad.setNombre("Nombre");
+        clienteMenorDeEdad.setApellido("Apellido");
+        clienteMenorDeEdad.setCedula("123456");
+        clienteMenorDeEdad.setTelefono("987654321");
+        clienteMenorDeEdad.setCorreoElectronico("cliente@gmail.com");
+        clienteMenorDeEdad.setEstatura(1.75);
+        clienteMenorDeEdad.setEdad(16);
+
+        ApiRequestException exception = assertThrows(ApiRequestException.class, () -> {
+            clienteServiceImp.crearCliente(clienteMenorDeEdad);
+        });
+
+        assertNotNull(exception);
+
+        assertTrue(exception instanceof ApiRequestException);
+
+        if (exception.getMessage() != null) {
+            assertTrue(exception.getMessage().contains("El Cliente Debe Tener Un Responsable si es menor de edad"));
+        }
+        verify(clienteRepository, never()).save(any(Cliente.class));
+    }
+
+}
---
+++ src/test/java/PruebaTecnica/RetoSofka/Service/EmpleadoServiceTest.java
@@ -0,0 +1,44 @@
+package PruebaTecnica.RetoSofka.Service;
+import PruebaTecnica.RetoSofka.Exception.ApiRequestException;
+import PruebaTecnica.RetoSofka.Modelo.Empleado;
+import PruebaTecnica.RetoSofka.Repository.EmpleadoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class EmpleadoServiceTest {
+
+    @Mock
+    private EmpleadoRepository empleadoRepository;
+
+    @InjectMocks
+    private EmpleadoService empleadoService;
+
+    @Test
+    void testCrearEmpleado() throws ApiRequestException {
+
+        Empleado empleado = new Empleado(123,"Juan", Empleado.TipoEmpleado.OPERADOR, "9:00 AM - 6:00 PM");
+
+        when(empleadoRepository.save(any(Empleado.class))).thenReturn(empleado);
+
+        Empleado resultado = empleadoService.crearEmpleado(empleado);
+
+        verify(empleadoRepository, times(1)).save(any(Empleado.class));
+        assertNotNull(resultado);
+        assertEquals("Juan", resultado.getNombre());
+        assertEquals(Empleado.TipoEmpleado.OPERADOR, resultado.getTipo());
+
+    }
+
+
+}
+
+
